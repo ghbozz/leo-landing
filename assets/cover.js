451b1f0bacf88ed4e68bd275cf9f6ca93a0a171d
romain sanson <romainsanson.gm@gmail.com>
2019-08-28 01:52:33 +0200

    working on responsiv

--- assets/cover.js
+++ assets/cover.js
@@ -1,13 +1,5 @@
-// const initWall = () => {
-//   const covers = document.querySelectorAll('.cover-columns .cover')
-//   covers.forEach((cover) => {
-//   setTimeout(translate(cover), 1000)
-//   })
-// }
-
-// const translate = (cover) => {
-//   // cover.classList.remove('translate')
-//   console.log(cover)
-// }
-
-// initWall();
+// $('.slider').slick({
+//   infinite: true,
+//   slidesToShow: 2,
+//   slidesToScroll: 3
+// });
