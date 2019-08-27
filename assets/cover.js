18c4e8e821612f2735d9d317d266142e861a724d
romain sanson <romainsanson.gm@gmail.com>
2019-08-27 10:42:20 +0200

    music front

---
+++ assets/cover.js
@@ -0,0 +1,13 @@
+// const initWall = () => {
+//   const covers = document.querySelectorAll('.cover-columns .cover')
+//   covers.forEach((cover) => {
+//   setTimeout(translate(cover), 1000)
+//   })
+// }
+
+// const translate = (cover) => {
+//   // cover.classList.remove('translate')
+//   console.log(cover)
+// }
+
+// initWall();
