68bf06c48fb48f72345b277372e442c8f009d1d5
romain sanson <romainsanson.gm@gmail.com>
2019-08-28 23:10:24 +0200

    css

--- assets/owl.js
+++ assets/owl.js
@@ -2,6 +2,9 @@ $(document).ready(function(){
   $('.owl-carousel').owlCarousel({
       items:1,
       margin:10,
+      autoplay:true,
+      loop:true,
+      smartSpeed: 800,
       autoHeight:true
   });
 });
